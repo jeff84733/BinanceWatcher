47992596b9bc042ac6edac7ccceb9bafc5fa8072
jeff.chen <jeff84733@hotmail.com>
2020-08-24 10:02:55 +0800

    add: 排序 add: 自動刷新 add: config.ini存座標 modify: 勾選自動刷新的時候，現在會立即刷新一次，再啟動Timer modify: 修改排序方式時，現在會立即更新排序

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,9 @@
 CD /D %~dp0
-SET PATH=C:\Qt\Qt5.9.9\5.9.9\msvc2015_64\bin;%PATH%
+SET PATH=C:\Qt\Qt5.9.8\5.9.8\msvc2015_64\bin;%PATH%
 windeployqt --plugindir "./release/plugins" --no-translations "./release/BinanceWatcher.exe"
 
 COPY /Y .\bin\* .\release\*
+REN ".\release" "BinanceWatcher"
+"C:\Program Files\WinRAR\WinRAR.exe" a -afzip -m5 -ag_YYYYMMDDHHMMSS ".\BinanceWatcher.zip" ".\BinanceWatcher"
+REN ".\BinanceWatcher" "release"
 PAUSE
--- widget.h
+++ widget.h
@@ -5,31 +5,103 @@
 #include <QTableWidget>
 #include <QScrollArea>
 #include <QScrollBar>
+#include <QLabel>
+#include <QCheckBox>
+#include <QComboBox>
 #include <QNetworkRequest>
 #include <QNetworkReply>
 #include <QNetworkAccessManager>
-#include <QLabel>
+#include <QTimer>
+
+
+
+typedef struct _PAIR_PROPERTY_NAME {
+    double dLastPrice;
+    double dPriceChangePercent;
+    double dHighPrice;
+    double dLowPrice;
+    double dWeightedAvgPrice;
+    double dQuoteVolume;
+} PAIR_PROPERTY_NAME;
+
+typedef union _PAIR_PROPERTY {
+    PAIR_PROPERTY_NAME name;
+    double id[6];
+} PAIR_PROPERTY;
+
+typedef struct _PAIR_INFO
+{
+    int id;
+    QString szSymbol;
+    QString szBaseAsset;
+    QString szQuoteAsset;
+    PAIR_PROPERTY propertys;
+} PAIR_INFO;
+
+
 
 class Widget : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit Widget(QWidget *parent = 0);
+    explicit Widget(QWidget *parent=nullptr);
     ~Widget();
 
     QTableWidget* page;
+    QList<PAIR_INFO> pairInfos;
+    QList<const PAIR_INFO*> pairRanks;
+    QTimer tmrAutoRefresh;
+    QTimer tmrRefreshPairInfos;
+    QCheckBox* uiAutoRefresh;
+    QComboBox* uiSortType;
 
 public slots:
+    void getApi_EXCHANGEINFO();
     void getApi_PRICE();
     void getApi_PRICE24HR();
+    void onApi_EXCHANGEINFO(QNetworkReply* reply);
     void onApi_PRICE(QNetworkReply* reply);
     void onApi_PRICE24HR(QNetworkReply* reply);
+    void refreshPairRanks();
+    void onUiAutoRefreshstateChanged(int state);
+    void onUiSortTypeCurrentIndexChanged(int index);
+    void refreshUi();
     void editConfig();
     void requestFinished(QNetworkReply* reply);
+
+protected:
+    void customEvent(QEvent* event);
+    void closeEvent(QCloseEvent* event);
 
 private:
     QNetworkAccessManager* nam;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif // WIDGET_H
