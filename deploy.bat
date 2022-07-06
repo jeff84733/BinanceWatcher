d7dc44d74c8c6b37a3c9ebe82ec3cdca3d0958fe
jeff_chen <jeff_chen@e-signal.com.tw>
2022-07-06 11:43:01 +0800

    fix: ssl depends.

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,8 @@
 CD /D %~dp0
-SET PATH=C:\Qt\Qt5.9.8\5.9.8\msvc2015_64\bin;%PATH%
-windeployqt --plugindir "./release/plugins" --no-translations "./release/BinanceWatcher.exe"
+REM SET PATH=C:\Qt\Qt5.9.8\5.9.8\msvc2015_64\bin;%PATH%
+REM windeployqt --plugindir "./release/plugins" --no-translations "./release/BinanceWatcher.exe"
 
-COPY /Y .\bin\* .\release\*
+REM COPY /Y .\bin\* .\release\*
 REN ".\release" "BinanceWatcher"
 "C:\Program Files\WinRAR\WinRAR.exe" a -afzip -m5 -ag_YYYYMMDDHHMMSS ".\BinanceWatcher.zip" ".\BinanceWatcher"
 REN ".\BinanceWatcher" "release"
